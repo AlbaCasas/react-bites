68a8e05e33b8d93185e2d2cd4f8add45579f45a0
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-05 16:23:16 +0100

    Add links to social icons

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,8 +7,20 @@ const Layout = ({ children }) => {
       <header className="layout__header">
         <h1 className="layout__title">React Challenges</h1>
         <div className="layout__icons">
-          <FaGithub className="layout__icon layout__icon--github" />
-          <FaLinkedin className="layout__icon" />
+          <a
+            href="https://github.com/AlbaCasas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub className="layout__icon layout__icon--github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/alba-casas/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin className="layout__icon" />
+          </a>
         </div>
       </header>
       <main className="layout__main">{children}</main>

8d3683c7ff2e4ae4ec96aa29bfd648c2af16ed90
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-07 18:18:22 +0100

    Input validation

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,9 +1,10 @@
-const Input = ({ placeholder, className = "", error }) => {
+const Input = ({ placeholder, className = "", error, onBlur }) => {
   return (
     <div>
       <input
         className={`input ${error ? "input--error" : ""}`}
         placeholder={placeholder}
+        onBlur={onBlur}
       />
       <p className="text--error">{error}</p>
     </div>
--- src/pages/challenges/Color/Color.jsx
+++ src/pages/challenges/Color/Color.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react/cjs/react.development";
 import Challenge from "../../../components/Challenge";
 import Input from "../../../components/Input";
 
+function getError(input) {
+  if (!input || input === "red" || input === "green" || input === "blue") {
+    return null;
+  }
+  return "Enter red, green or blue";
+}
+
 const Color = () => {
+  const [input, setInput] = useState(null);
+
   return (
     <div>
       <Challenge title="Type a color">
         Write a “red”, “green” or “blue” in the input. The box will change its
-        background correspondingly. If you write an invalid color, the input
-        will show the helper text: “Incorrect color”
+        background correspondingly <u>when you click outside the input</u>. If
+        you write an invalid color, the input will show the helper text: “Enter
+        red, green or blue”
       </Challenge>
       <div className="color__solution">
         <Input
-          error="Enter red, green or blue"
+          onBlur={(e) => {
+            setInput(e.target.value);
+          }}
+          error={getError(input)}
           placeholder="Write red, green or blue"
         />
         <div className="color__box" />

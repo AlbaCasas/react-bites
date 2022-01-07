90d2aa57048e396acf07ee54ed8d3b24fc9a0c5e
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-07 19:36:22 +0100

    hotfix import

--- src/pages/challenges/Color/Color.jsx
+++ src/pages/challenges/Color/Color.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import Challenge from "../../../components/Challenge";
 import Input from "../../../components/Input";
 
